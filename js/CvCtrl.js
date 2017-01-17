95f888f00d41365f78a737740ca5bcb35f321c87
sergii4 <getman.sergei@gmail.com>
2017-01-17 15:13:29 +0200

    Update CvCtrl.js

--- js/CvCtrl.js
+++ js/CvCtrl.js
@@ -2,18 +2,22 @@ cvApp.controller('CvCtrl', ['$scope', function($scope) {
 	$scope.projects = [
     {'company': 'Dubit',
      'name': 'Dubit',
-     'desc': 'Different small Rest Api projects based on Spring Boot and wrapped in Docker container'
+     'desc': 'Different small Rest Api projects based on Spring Boot and wrapped in Docker container',
+     'date': '2016 - present time'
  	},
     {'company': 'Levi9',
      'name': 'Backbase',
-     'desc': 'Backbase CXP is a loosely coupled presentation layer that sits on top of your existing systems. It helps you to compose truly customer-centric dialogs across any device, and combines content, data, and functionality from different underlying systems (silos) into a new, fresh, UX presentation layer. Backbase CXP is based on ‘lean architecture’ principles and, unlike traditional portals, is up and running within months, not years.'
+     'desc': 'Backbase CXP is a loosely coupled presentation layer that sits on top of your existing systems. It helps you to compose truly customer-centric dialogs across any device, and combines content, data, and functionality from different underlying systems (silos) into a new, fresh, UX presentation layer. Backbase CXP is based on ‘lean architecture’ principles and, unlike traditional portals, is up and running within months, not years.',
+     'date': '2014 - 2015'
  	},
     {'company':'GlobalLogic',
      'name': 'Bottomline',
-     'desc': 'Online banking payment system for all types UK payments'},
+     'desc': 'Online banking payment system for all types UK payments',
+     'date': '2013 - 2014'},
     {'company':'Epam',
      'name': 'Sephora',
-     'desc': 'Discount system for Sephora(perfume retailer). Main application (Sephora) communicate with discount system(Beauty Insider) via Web Services. Beauty Insider is responsible for all loyalty business logic.'}
+     'desc': 'Discount system for Sephora(perfume retailer). Main application (Sephora) communicate with discount system(Beauty Insider) via Web Services. Beauty Insider is responsible for all loyalty business logic.',
+     'date': '2010 - 2013'}
   ];
 
   $scope.edus = [
