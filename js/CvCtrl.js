fbd6a293bd142b4862607fb0f1b5946a99ed888e
sergii4 <getman.sergei@gmail.com>
2017-09-06 11:16:28 +0300

    Update CvCtrl.js

--- js/CvCtrl.js
+++ js/CvCtrl.js
@@ -1,39 +1,52 @@
 cvApp.controller('CvCtrl', ['$scope', function($scope) { 
 	$scope.projects = [
+    {'company': 'GlobalLogic',
+     'name': 'Royal Flora Holland',
+     'desc': 'Big e-commerce platform for plants growers and customers',
+     'date': '2017 - present time',
+     'position': 'java developer',
+     'responsibilities': 'develop app from sratch, providing estimation, working on app architecture, implementation user stories',
+     'techologies': 'CQRS, Elasticsearch, Spring, Microservices, Axon, Docker, AWS'
+ 	},
     {'company': 'Dubit',
      'name': 'Dubit',
      'desc': 'Different small Rest Api projects based on Spring Boot and wrapped in Docker container',
-     'date': '2016 - present time',
+     'date': '2016 - 2017',
      'position': 'java developer',
-     'responsibilities': 'develop app from sratch, providing estimation, working on app architecture, implementation user stories'
+     'responsibilities': 'develop app from sratch, providing estimation, working on app architecture, implementation user stories',
+     'techologies': 'Spring, Microservices, Docker, AWS'
  	},
     {'company': 'Levi9',
      'name': 'Backbase',
      'desc': 'Backbase CXP is a loosely coupled presentation layer that sits on top of your existing systems. It helps you to compose truly customer-centric dialogs across any device, and combines content, data, and functionality from different underlying systems (silos) into a new, fresh, UX presentation layer. Backbase CXP is based on ‘lean architecture’ principles and, unlike traditional portals, is up and running within months, not years.',
      'date': '2014 - 2015',
      'position': 'java developer',
-     'responsibilities': 'working on app architecture, implementation user stories, bug fixing, maintatinance'
+     'responsibilities': 'working on app architecture, implementation user stories, bug fixing, maintatinance',
+     'techologies': 'Spring, Apache Camel'
  	},
     {'company':'GlobalLogic',
      'name': 'Bottomline',
      'desc': 'Online banking payment system for all types UK payments',
      'date': '2013 - 2014',
      'position': 'java developer',
-     'responsibilities': 'develop app from sratch, providing estimation, implementation user stories, bug fixing'
+     'responsibilities': 'develop app from sratch, providing estimation, implementation user stories, bug fixing',
+     'techologies': 'Spring, Jax-Rs'
     },
     {'company':'Epam',
      'name': 'Sephora',
      'desc': 'Discount system for Sephora(perfume retailer). Main application (Sephora) communicate with discount system(Beauty Insider) via Web Services. Beauty Insider is responsible for all loyalty business logic.',
      'date': '2012 - 2013',
      'position': 'java developer',
-     'responsibilities': 'implementation user stories, bug fixing, maintainance, writing js, sql scripts'
+     'responsibilities': 'implementation user stories, bug fixing, maintainance, writing js, sql scripts',
+     'techologies': 'EJB 2, JSP, Servlets, WebLogic'
     },
     {'company':'Epam',
      'name': 'Octopustravel',
      'desc': 'System provides hotels search and booking functionality across one of the direct or partner brands, through web or XML interface.',
      'date': '2010 - 2012',
      'position': 'java developer',
-     'responsibilities': 'adding new functionality, bug fixing, working on infrastructure'
+     'responsibilities': 'adding new functionality, bug fixing, working on infrastructure',
+     'techologies': 'Spring, Tomcat, JSP, Servlets'
     }
   ];
 
